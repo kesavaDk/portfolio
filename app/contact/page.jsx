6a84000c0f9d146a9cc646adaa2c121c9116d2d1
kesavaDk <kesavad1652@gmail.com>
2024-06-23 22:15:36 +0530

    refactoring the code

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -47,11 +47,10 @@ const Contact = () => {
         <div className="flex flex-col xl:flex-row gap[30px]">
           <div className="xl:w-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent">Let's work together</h3>
-              <p className="text-white/60">
-                lerem ipsum dolor sit amet consecterur adipisicing elit. Enum
-                nihil sapiente pariatur id totam.
-              </p>
+              <h3 className="text-4xl text-accent">Lets work together</h3>
+              {/* <p className="text-white/60">
+               
+              </p> */}
               <div className="grid grid-cols md:grid-cols-2 gap-6">
                 <Input type="firstname" placeholder="Firstname" />
                 <Input type="lastname" placeholder="Lastname" />
